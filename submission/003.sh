7d8fdab00abe4431c5a2e18b21e33adc0d01b416
costa.moises <moises.costa@syonet.com>
2024-11-21 10:12:44 -0300

    challenge completion

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,10 @@
 # How many new outputs were created by block 123,456?
+
+# Obtém o hash do bloco 123,456
+block_hash=$(bitcoin-cli getblockhash 123456)
+
+# Obtém os dados completos do bloco no formato JSON
+block=$(bitcoin-cli getblock $block_hash 2)
+
+# Conta o número total de "vout" no JSON usando jq
+echo "$block" | jq '[.tx[].vout] | flatten | length'
--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,25 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+# Bloco especificado
+block=257343
+
+# Obter bloco anterior
+prev_block=256128
+
+# Obter hash do bloco anterior
+prev_block_hash=$(bitcoin-cli getblockhash $prev_block)
+
+# Obter transação coinbase do bloco anterior
+prev_coinbase_txid=$(bitcoin-cli getblock $prev_block_hash 1 | jq -r '.tx[0]')
+
+# Obter bloco atual
+block_hash=$(bitcoin-cli getblockhash $block)
+
+# Buscar transação que gasta o coinbase
+for tx in $(bitcoin-cli getblock $block_hash 2 | jq -r '.tx[].txid'); do
+    inputs=$(bitcoin-cli getrawtransaction $tx 1 | jq '.vin')
+    if echo "$inputs" | grep -q "$prev_coinbase_txid"; then
+        echo $tx
+        break
+    fi
+done
